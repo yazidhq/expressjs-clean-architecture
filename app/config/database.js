f71471bc78e0ab2217cca6bd93b6f6c41ed62d68
yazid dhiaulhaq ismail <yaziddhiaulhaqismail@yahoo.co.id>
2025-05-22 09:56:06 +0700

    fix: database config

--- app/config/database.js
+++ app/config/database.js
@@ -1,7 +1,6 @@
 const { Sequelize } = require("sequelize");
-const { development } = require("./db.config");
-
-const env = process.env.NODE_ENV || development;
 const config = require("./db.config");
+
+const env = process.env.NODE_ENV || "development";
 
 exports.sequelize = new Sequelize(config[env]);
